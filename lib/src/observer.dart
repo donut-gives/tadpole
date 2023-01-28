5bb5f0440e3ffdfc905a17a06f76b834441443c1
Ayush Suman <ayush231suman@gmail.com>
2023-01-29 00:51:18 +0530

    Fix Issue in Observer

--- lib/src/observer.dart
+++ lib/src/observer.dart
@@ -6,17 +6,21 @@ class Observer<L> extends StatefulWidget {
   const Observer({Key? key, this.child, required this.builder, required this.liveNotifier}) : super(key: key);
 
   final LiveNotifier<L> liveNotifier;
-  final Widget Function(BuildContext context, L value, Widget? child) builder;
+  final Widget Function<L>(BuildContext context, L value, Widget? child) builder;
   final Widget? child;
 
   @override
-  State<Observer> createState() => _ObserverState();
+  State<Observer> createState() => _ObserverState<L>();
 }
 
-class _ObserverState extends State<Observer> with LifeCycleOwnerMixin {
+class _ObserverState<L> extends State<Observer<L>> with LifeCycleOwnerMixin {
+  late L value;
   @override
   void initState() {
-    widget.liveNotifier.observe(lifeCycleOwner, () {setState((){});});
+    value = widget.liveNotifier.value;
+    widget.liveNotifier.observe(lifeCycleOwner, () {setState((){
+      value = widget.liveNotifier.value;
+    });});
     super.initState();
   }
 
